2b0921df82c6ecfa6a05e2dd9d7a9c07a2b798ff
Rafael Tito <rafael.titom2007@gmail.com>
2026-06-13 17:23:24 -0500

    version_2

--- index.cpp
+++ index.cpp
@@ -1,8 +1,71 @@
+#include <consoleapi2.h>
 #include <iostream>
+#include <Windows.h>
 
 using namespace std;
 
+//pantallasos
+void Inicio();
+void ganaste();
+void perdiste();
+
+void Gotoxy(int x, int y);
+
+
 int main() {
-    cout << "hello world";
+    int vidas, tiempo, salvados;    
+    bool x;
+
+    Inicio();
+
     return 0;
+}
+
+void Gotoxy(int x, int y) {
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(HANDLE STD_OUTPUT_HANDLE, coord);
+}
+
+void Inicio() {
+    cout << "######################################################\n";
+    cout << "######################################################\n";
+    cout << "                                                      \n";
+    cout << "                                                      \n";
+    cout << "                 RESCUE TO THE MOON                   \n";
+    cout << "                                                      \n";
+    cout << "         JUGAR?          SI           NO              \n";
+    cout << "                                                      \n";
+    cout << "                                                      \n";
+    cout << "######################################################\n";
+    cout << "######################################################\n";
+}
+
+void ganaste() {
+    cout << "######################################################\n";
+    cout << "######################################################\n";
+    cout << "                                                      \n";
+    cout << "                                                      \n";
+    cout << "                       GANASTE                        \n";
+    cout << "                                                      \n";
+    cout << "         DE NUEVO?          SI           NO           \n";
+    cout << "                                                      \n";
+    cout << "                                                      \n";
+    cout << "######################################################\n";
+    cout << "######################################################\n";
+}
+
+void perdiste() {
+    cout << "######################################################\n";
+    cout << "######################################################\n";
+    cout << "                                                      \n";
+    cout << "                                                      \n";
+    cout << "                      PERDISTE                        \n";
+    cout << "                                                      \n";
+    cout << "         DE NUEVO?          SI           NO           \n";
+    cout << "                                                      \n";
+    cout << "                                                      \n";
+    cout << "######################################################\n";
+    cout << "######################################################\n";
 }
